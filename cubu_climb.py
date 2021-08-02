d52166227c9b83e2e6b79b9bbad3a517a17e723d
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-08-02 22:58:41 +0300

    Add files via upload

---
+++ cubu_climb.py
@@ -0,0 +1,37 @@
+import from 123cube.h
+
+
+add form docreader : 0
+	null 
+	as 1-1
+	   2-2
+	   3-3
+	   4-4
+	   5-5
+	   6-6
+	   7-7
+hop ration int 
+	null = 0
+	dice = odd 
+	status = reader
+	/.
+doc empty 
+./n
+	full hear recycle moder 
+		conjugate : ("/223231351/3125341254152/54321.cpp")
+		int 
+
+		2-1
+ 		1-2
+	        3-2
+	        2-3
+	        3-4
+	        4-3
+	        5-4
+	        4-5
+on
+in:
+ readable abuduncy curt 
+	to full mode ("/.cpp")
+	("%12 to %-12")
+NOW
