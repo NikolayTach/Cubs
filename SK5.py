4994170cedfdbba5da08135f20ee5b127becfe5b
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-08-03 20:37:57 +0300

    Add files via upload

---
+++ SK5.py
@@ -0,0 +1,32 @@
+include __FACE__
+
+main face(0)
+print('link:null')
+in face('happy';'mad';'sad')
+
+new_load('ssd')
+or
+MAD_Y1[type @log1]
+MAD_Y2[type @log11]
+MAD_Y3[type @log21]
+MAD_Y4[type @log31]
+MAD_Y5[type @log41]
+MAD_Y6[type @log51]
+MAD_Y7[type @log61]
+MAD_Y8[type @log71]
+MAD_Y9[type @log81]
+MAD_Y10[type @log91]
+MAD_Y11[type @log101]
+MAD_Y12[type @log111]
+MAD_Y13[type @log121] 
+
+is ('link:full') as
+main face(1)
+do queary_range:2,3,4,5,6,7,8,10 
+from s/asd/roor.d
+side
+\
+MAD_Y[type @log1]
+range Person2 Animal12
+\
+
---
+++ sas.py
@@ -0,0 +1,31 @@
+import soap
+import loading_1
+
+pick 0.as(0)
+	asp_function
+./n
+	cz_select
+./n
+	same
+	./n
+		null
+	./n
+	pair_child(1)
+		import panda&cows
+	patch formation int B_function as asp_function
+	./n
+			core all fuction 
+			print('^AD^AD^')
+			print('^GF^GF^')
+			mask KDE_@tools 
+		pick 0.as(0)
+	asp_function
+	./n
+	cz_select
+./n
+	same
+	./n
+		null
+		./n
+		pair_child(2)
+		
