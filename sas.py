903cdee09d16ffacd908bc737dc0dd01f57e1f2e
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-11-05 05:11:23 -0700

    Update sas.py

--- sas.py
+++ sas.py
@@ -3,29 +3,29 @@ import loading_1
 
 pick 0.as(0)
 	asp_function
-./n
+'\n'
 	cz_select
-./n
+'\n'
 	same
-	./n
+	'\n'
 		null
-	./n
+	'\n'
 	pair_child(1)
 		import panda&cows
 	patch formation int B_function as asp_function
-	./n
+	'\n'
 			core all fuction 
 			print('^AD^AD^')
 			print('^GF^GF^')
 			mask KDE_@tools 
 		pick 0.as(0)
 	asp_function
-	./n
-	cz_select
-./n
+'\n'
+cz_select
+'\n'
 	same
-	./n
-		null
-		./n
+'\n'
+null
+		'\n'
 		pair_child(2)
 		
